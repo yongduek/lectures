b9d7099c2fe155a98b0d5daf02ca131bc798955f
yongduek.seo <yongduek.seo@gmail.com>
2019-04-16 00:04:38 +0900

    morph

---
+++ opencv-python/python/morph-dilation.py
@@ -0,0 +1,11 @@
+import os
+import imageio 
+import matplotlib.pyplot as plt
+from skimage.morphology import erosion, dilation, opening, closing, white_tophat
+from skimage.morphology import black_tophat, skeletonize, convex_hull_image
+from skimage.morphology import disk
+
+selem = disk(6)
+print (type(selem), selem.dtype, selem.shape, '\n', selem)
+plt.imshow (selem, cmap='gray')
+plt.pause(2)
