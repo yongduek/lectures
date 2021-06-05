c5c0c17256322bffe31aebb625ed236520645001
Yongduek Seo <yongduek.seo@gmail.com>
2021-06-06 01:15:35 +0900

    iris

---
+++ rl/pygame/Pong_101computing.net/paddle.py
@@ -0,0 +1,66 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+import pygame
+BLACK = (0,0,0)
+ 
+class Paddle(pygame.sprite.Sprite):
+    #This class represents a car. It derives from the "Sprite" class in Pygame.
+    
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+ 
+        # Draw the paddle (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
+        #Check that you are not going too far (off the screen)
+        if self.rect.y < 0:
+          self.rect.y = 0
+          
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+        #Check that you are not going too far (off the screen)
+        if self.rect.y > 400:
+          self.rect.y = 400
