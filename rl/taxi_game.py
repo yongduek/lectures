2720e238b1b128a0c1700771ebb4e177cebd2af9
Yongduek <yongduek.seo@gmail.com>
2021-07-01 10:37:19 +0900

    taxi

--- rl/taxi_game.py
+++ rl/taxi_game.py
@@ -38,15 +38,45 @@ arrow_keys = {
     'o' : DROPOFF
 }
 
+"""
+# Encoding/Decoding Information
+
+Passenger locations:
+    - 0: R(ed)
+    - 1: G(reen)
+    - 2: Y(ellow)
+    - 3: B(lue)
+    - 4: in taxi
+
+Destinations:
+    - 0: R(ed)
+    - 1: G(reen)
+    - 2: Y(ellow)
+    - 3: B(lue)
+"""
+
+# State Decoding: taxi_row, taxi_col, pass_loc, dest_idx
+def decode(dec_state):  # input: env.decode(state)
+    dec_state = list(dec_state)
+    names = ["taxi_row", "taxi_col", "pass_loc", "dest_idx"]
+    pass_loc = {0: 'R', 1: 'G', 2: 'Y', 3: 'B', 4: 'Taxi'}
+    decoded = { "taxi" : (dec_state[0], dec_state[1]),
+                "pass" : pass_loc[dec_state[2]],
+                "dest" : pass_loc[dec_state[3]]}  # the same as pass_loc
+    # decoded = {k: v for k, v in zip(names, dec_state)}
+    return decoded
+#
+
 # Gym 환경설정
 # https://gym.openai.com/envs/ 에 들어가면 다양한 환경을 살펴볼 수 있다.
-# Frozen-Lake : https://gym.openai.com/envs/FrozenLake-v0/ 
 
 env = gym.make("Taxi-v3")
 
 state = env.reset()
 env.render()
+print(f'returned: {state}', 'decode: ', decode(env.decode(state)))
 
+G = 0  # total return
 while True:
     # 키 값을 기다린다
     key = inkey()
@@ -62,14 +92,17 @@ while True:
     # 행동의 결과로 이동한 자리가 어딘지(state), 행동의 reward는 얼마인지(reward), 
     # 게임이 끝났는지: 구멍에 빠지거나 결승점에 도착하거나 (done), 그 밖의 정보 (info)가 리턴된다.
     state, reward, done, info = env.step(action)
+    G += reward 
 
     # 화면에 Frozen Lake 맵과 현재 위치가 어딘지 그림을 그려줌 (Frozen Lake는 콘솔에 출력)
     env.render()
-    print(f'{state} {reward} {done} {info}')
+
+    print(f'returned: {state} {reward} {done} {info}', '\n'
+            'decode: ', decode(env.decode(state)))
 
     # 게임이 끝났으면 Reward를 출력
     if done :
-        print("Finished with reward {0}".format(reward))
+        print(f"Finished with total return {G}")
         print(" ---------  New Game ----------")
         # repeat the game
         state = env.reset()
